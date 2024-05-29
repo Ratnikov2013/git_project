0bbcde63c5bf9e47e5cdca5cbe5a8bf4ebce99a1
Ivan Ratnikov <ratnikov_iv@mail.ru>
2024-05-29 14:11:29 +0200

    two_numbers

--- funcs.py
+++ funcs.py
@@ -1,9 +1,6 @@
-def two_numbers():
-    a = int()
-    b = int()
+def two_numbers(a, b):
+
     return a + b
 
-def difference_of_two_numbers():
-    c = int()
-    d = int()
+def difference_of_two_numbers(c, d):
     return c - d
