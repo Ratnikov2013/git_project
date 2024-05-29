02caeff0b60f4d42b702299b144c90ac05b32e59
Ivan Ratnikov <ratnikov_iv@mail.ru>
2024-05-29 14:33:34 +0200

    main_import

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from adding_two_mumbers import two_numbers
+from funcs import two_numbers
+from funcs import difference_of_two_numbers
+a = 5
+b = 5
+c = 10
+d = 4
+result_subtraction = difference_of_two_numbers(c, d)
+result_add = two_numbers(a, b)
+print(result_add)
+
+print(result_subtraction)
