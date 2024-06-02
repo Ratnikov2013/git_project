cf95e80dd9dffeb6b4a2a3738bc7b29d6a37781b
Ratnikov2013 <ratnikov_iv@mail.ru>
2024-06-02 11:26:50 +0200

    main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 def print_world():
     print('Hello Linux')
+=======
+>>>>>>> parent of 40496c5 (Hello in main.py)
 =======
 >>>>>>> parent of 40496c5 (Hello in main.py)
